0d44a430de4ce77b73961d36f599f8722e1864da
mhmddev <mabrahimibagha@gmail.com>
2023-04-12 14:36:26 +0330

    perf: changed style

--- components/player/player.jsx
+++ components/player/player.jsx
@@ -3,7 +3,7 @@ import Player from "@/helpers/player";
 const PlayerView = ({src}) => {
     return (
         <>
-            <div className="border-t py-5 px-14">
+            <div className="border-t py-10 px-14">
                 <Player src={src}/>
             </div>
         </>
