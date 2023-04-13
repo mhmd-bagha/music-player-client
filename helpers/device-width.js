fd9302ad69e788a823b3dba0d5b5a8896855982a
mhmddev <mabrahimibagha@gmail.com>
2023-04-13 17:35:49 +0330

    perf: updated performance

--- helpers/device-width.js
+++ helpers/device-width.js
@@ -1,32 +1,30 @@
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useLayoutEffect, useRef, useState} from "react";
 
 const useDeviceWidth = () => {
-    const devices = {sm: 576, md: 768, lg: 992, xl: 1200, xxl: 1400};
     const [width, setWidth] = useState(typeof window !== "undefined" ? window.innerWidth : null);
+    const devices = useRef({sm: 576, md: 768, lg: 992, xl: 1200, xxl: 1400});
 
     const getDevice = useCallback(() => {
         switch (true) {
-            case width >= devices.xxl:
+            case width >= devices.current.xxl:
                 return "xxl";
-            case width >= devices.xl:
+            case width >= devices.current.xl:
                 return "xl";
-            case width >= devices.lg:
+            case width >= devices.current.lg:
                 return "lg";
-            case width >= devices.md:
+            case width >= devices.current.md:
                 return "md";
-            case width >= devices.sm:
+            case width >= devices.current.sm:
                 return "sm";
         }
-    }, [devices.lg, devices.md, devices.sm, devices.xl, devices.xxl, width]);
+    }, [width]);
 
-    const handleResize = () => setWidth(window.innerWidth);
+    useLayoutEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        handleResize();
 
-    useEffect(() => {
         window.addEventListener("resize", handleResize);
-
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
+        return () => window.removeEventListener("resize", handleResize);
     }, []);
 
     return {devices, getDevice, width};
