9af0d2cf85f7c6d43668f6edb1c6029f26103b1d
mhmddev <mabrahimibagha@gmail.com>
2023-05-07 22:19:34 +0330

    feat: added artist image and name

---
+++ components/artist/artist-image.tsx
@@ -0,0 +1,20 @@
+const ArtistImage = () => {
+    return (
+        <div className="relative">
+            <div className="flex items-center">
+                <div className="absolute top-0 left-0 w-full h-full bg-stone-900 bg-opacity-90"/>
+                <ArtistImage src="/albums/album.jpg" alt="archive album" className="w-full h-96 object-cover" width={2000}
+                             height={2000}/>
+                <ArtistImage src="/albums/album.jpg" alt="archive album"
+                             className="w-48 h-48 absolute rounded-md object-cover ml-6" width={2000} height={2000}/>
+            </div>
+            <div className="absolute top-36 left-48 pl-14 z-10">
+                <p className="text-white">Playlist</p>
+                <p className="text-white text-5xl font-black mt-4">archive</p>
+                <p className="text-white mt-8">315,136 monthly listeners</p>
+            </div>
+        </div>
+    )
+}
+
+export default ArtistImage
--- pages/album/[album-name]/[album-id].tsx
+++ pages/album/[album-name]/[album-id].tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
+import ArtistImage from "Component/artist/artist-image";
 
 const Album = () => {
     return (
         <>
-            <div className="relative">
-                <Image src="/albums/album.jpg" alt="archive album" className="w-full h-80" width={2000} height={2000}/>
-                <p className="text-lg font-bold">archive</p>
-            </div>
+            <ArtistImage/>
         </>
     )
 }
