a22c849e77921722949878050c7348d1952045fb
mhmddev <mabrahimibagha@gmail.com>
2023-05-08 19:04:55 +0330

    fix: replaced component to Image

--- components/artist/artist-image.tsx
+++ components/artist/artist-image.tsx
@@ -1,11 +1,13 @@
+import Image from "next/image";
+
 const ArtistImage = () => {
     return (
         <div className="relative">
             <div className="flex items-center">
                 <div className="absolute top-0 left-0 w-full h-full bg-stone-900 bg-opacity-90"/>
-                <ArtistImage src="/albums/album.jpg" alt="archive album" className="w-full h-96 object-cover" width={2000}
+                <Image src="/albums/album.jpg" alt="archive album" className="w-full h-96 object-cover" width={2000}
                              height={2000}/>
-                <ArtistImage src="/albums/album.jpg" alt="archive album"
+                <Image src="/albums/album.jpg" alt="archive album"
                              className="w-48 h-48 absolute rounded-md object-cover ml-6" width={2000} height={2000}/>
             </div>
             <div className="absolute top-36 left-48 pl-14 z-10">
