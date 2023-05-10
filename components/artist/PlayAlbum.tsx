9028e1c18f4962b19e9e15cb037310dd78f6eb52
mhmddev <mabrahimibagha@gmail.com>
2023-05-10 10:16:03 +0330

    feat: play music and fllow and other icon in album

--- components/artist/PlayAlbum.tsx
+++ components/artist/PlayAlbum.tsx
@@ -1,13 +1,16 @@
 import {CiPlay1} from "react-icons/ci";
+import {BsThreeDots} from "react-icons/bs";
 
 const PlayAlbum = () => {
     return (
-        <div className="flex items-center px-3">
-            <button className="p-3 rounded-full">
-                <CiPlay1/>
+        <div className="flex items-center gap-10 px-5 py-5 bg-anti-flash-white">
+            <button className="bg-gunmetal p-3 rounded-full text-white hover:scale-105 transition-all duration-100">
+                <CiPlay1 size={25}/>
             </button>
-            <button></button>
-            <button></button>
+            <button className="py-1 px-6 bg-transparent border border-dark-electric-blue rounded hover:border-gunmetal">Follow</button>
+            <button className="text-dark-electric-blue hover:text-gunmetal" title='More options for Archive'>
+                <BsThreeDots size={25}/>
+            </button>
         </div>
     )
 }
