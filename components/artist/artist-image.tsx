a56baa7f6309ba94c4132ef3f93cc86dd8c7bf40
mhmddev <mabrahimibagha@gmail.com>
2023-05-12 09:09:43 +0330

    refactor: removed z index

--- components/artist/artist-image.tsx
+++ components/artist/artist-image.tsx
@@ -10,7 +10,7 @@ const ArtistImage = () => {
                 <Image src="/albums/album.jpg" alt="archive album"
                              className="w-48 h-48 absolute rounded-md object-cover ml-3 sm:ml-6" width={2000} height={2000}/>
             </div>
-            <div className="absolute top-36 left-48 pl-10 sm:pl-14 z-10">
+            <div className="absolute top-36 left-48 pl-10 sm:pl-14">
                 <p className="text-white">Playlist</p>
                 <p className="text-white text-4xl lg:text-5xl font-black mt-4">archive</p>
                 <p className="text-white mt-8">46,581 likes50 songs, <span className="text-gray-300">about 2 hr 15 min</span></p>
