ab2c7eacd41cee8150b2d2a321aaf64d059de7ea
mhmddev <mabrahimibagha@gmail.com>
2023-05-13 14:21:00 +0330

    feat: added player

--- pages/album/[album-name]/[album-id].tsx
+++ pages/album/[album-name]/[album-id].tsx
@@ -1,6 +1,7 @@
 import ArtistImage from "Component/artist/artist-image";
 import PlayAlbum from "Component/artist/PlayAlbum";
 import Songs from "Component/artist/songs";
+import PLayerView from "Component/player/player";
 
 const Album = () => {
     return (
@@ -8,6 +9,7 @@ const Album = () => {
             <ArtistImage/>
             <PlayAlbum/>
             <Songs/>
+            <PLayerView/>
         </>
     )
 }
