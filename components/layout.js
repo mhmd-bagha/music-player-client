1eda49a26d54f198ac450790316b4f61b190dada
mhmddev <mabrahimibagha@gmail.com>
2023-05-13 15:58:27 +0330

    perf: full width according to showSidebar

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,15 @@
 import Menu from "Component/menu/menu";
 import Sidebar from "Component/sidebar/sidebar";
+import {useGlobalContext} from "@/context/store";
 
 const Layout = ({children}) => {
+    const {showSidebar} = useGlobalContext()
+
     return (
         <>
             <div className="lg:flex">
                 <Menu/>
-                <div className="w-full lg:w-4/6">{children}</div>
+                <div className={showSidebar ? 'w-full lg:w-4/6' : 'w-full'}>{children}</div>
                 <Sidebar/>
             </div>
         </>
