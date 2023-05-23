2cd32f2bf9a4c1920e999ff018f9c27f914fc1b8
mhmddev <mabrahimibagha@gmail.com>
2023-05-23 21:40:58 +0330

    feat: completed login ui

--- components/auth/login.tsx
+++ components/auth/login.tsx
@@ -1,13 +1,20 @@
+import Link from "next/link";
 import {BiUser} from "react-icons/bi";
 import {AiOutlineLock} from "react-icons/ai";
 
 const Login = () => {
     return (
-        <div className="grid place-items-center h-screen">
-            <div className="shadow bg-white rounded-lg py-5 px-8 h-1/2">
+        <div className="grid place-items-center h-screen bg-anti-flash-white">
+            <div className="shadow bg-white rounded-xl py-10 px-8">
+                {/* title */}
+                <div className="text-center pb-6">
+                    <p className="text-lg text-gunmetal font-black">You must Sign In to join</p>
+                    <p className="text-sm text-dark-electric-blue capitalize">We're a team that guides each other</p>
+                </div>
+
                 <form>
                     {/* email */}
-                    <div className="py-2">
+                    <div className="py-3">
                         <label htmlFor="email" className="text-dark-electric-blue">Email</label>
                         <div className="flex items-center relative mt-2.5">
                             <BiUser size={22} className="text-zinc-400 absolute ml-2"/>
@@ -16,7 +23,7 @@ const Login = () => {
                         </div>
                     </div>
                     {/* password */}
-                    <div className="py-2">
+                    <div className="py-3">
                         <label htmlFor="password" className="text-dark-electric-blue">Password</label>
                         <div className="flex items-center relative mt-2.5">
                             <AiOutlineLock size={22} className="text-zinc-400 absolute ml-2"/>
@@ -24,6 +31,15 @@ const Login = () => {
                                    className="border border-zinc-300 py-2.5 px-10 rounded-lg outline-none placeholder:text-zinc-400 pl-10"/>
                         </div>
                     </div>
+                    {/* forgot password */}
+                    <Link href="/auth/forgot-password" className="text-sm text-gunmetal flex justify-end">
+                        Forgot password?
+                    </Link>
+                    {/* login button */}
+                    <button type="button"
+                            className="bg-gunmetal hover:bg-dark-electric-blue text-anti-flash-white w-full py-2.5 rounded-lg mt-10 transition">
+                        Sing in
+                    </button>
                 </form>
             </div>
         </div>
