eb43a131b15a4638d54cc3d041bec9a5900a7a6c
mhmddev <mabrahimibagha@gmail.com>
2023-06-01 21:28:52 +0330

    feat: added auth token to headers

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,5 @@
 import axiosInstance from './axios';
+import Cookies from 'js-cookie';
 
 interface loginData {
     email: string,
@@ -7,5 +8,9 @@ interface loginData {
 
 export const auth = async (data: loginData) => {
     const res = await axiosInstance.post('/auth/login', data);
+
+    if (res.status === 201 || res.status === 200)
+        Cookies.set('auth', res.data.token.access_token)
+
     return res.data
 }
