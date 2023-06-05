ad1809922809e15a85962478dfd96b53d5a31341
mhmddev <mabrahimibagha@gmail.com>
2023-06-05 12:51:08 +0330

    feat: add get user

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,6 @@
 import axiosInstance from './axios';
 import Cookies from 'js-cookie';
+import {userType} from "@/types/auth";
 
 interface loginData {
     email: string,
@@ -11,6 +12,12 @@ export const auth = async (data: loginData) => {
 
     if (res.status === 201 || res.status === 200)
         Cookies.set('auth', res.data.token.access_token)
+
+    return res.data
+}
+
+export const user = async (): Promise<userType> => {
+    const res = await axiosInstance.post('/auth/user');
 
     return res.data
 }
