bbb2d02214913d7e5024a0285c350c392128284f
mhmddev <mabrahimibagha@gmail.com>
2023-06-05 17:05:47 +0330

    feat: ad name

--- components/sidebar/profile.jsx
+++ components/sidebar/profile.jsx
@@ -2,9 +2,12 @@ import Image from "next/image";
 import ImagePerson from "@/public/users/elon-musk.jpg";
 import {IoNotificationsOutline} from "react-icons/io5";
 import {useState} from "react";
+import useUser from "@/hooks/auth";
+import Link from "next/link";
 
 const Profile = () => {
     const enableAlarm = useState(false)
+    const {user} = useUser()
 
     return (
         <div className="flex justify-between items-center">
@@ -12,7 +15,9 @@ const Profile = () => {
             <div className="flex items-center">
                 <Image src={ImagePerson} alt={'elon musk'} className="w-12 h-12 rounded-3xl"/>
                 <div className="pl-3.5 lg:hidden 2xl:block">
-                    <p className="color-gunmetal text-sm font-bold">elon musk</p>
+                    {(user.last_name !== null) ?
+                        <p className="color-gunmetal text-sm font-bold">{user.last_name}</p> :
+                        <Link href="/profile">Change name</Link>}
                     <p className="color-crayola text-sm xl:text-xs">Premium Member.</p>
                 </div>
             </div>
