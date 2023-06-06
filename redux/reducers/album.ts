083c65ae569955dd4fc63f01d23086d55b6b6324
mhmddev <mabrahimibagha@gmail.com>
2023-06-06 11:28:24 +0330

    feat(redux): add album

---
+++ redux/reducers/album.ts
@@ -0,0 +1,21 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+interface InitialState {
+
+}
+
+const initialState: InitialState = {
+    albums: []
+}
+
+const albumSlice = createSlice({
+    name: 'album',
+    initialState,
+    reducers: {
+        set: (state, action: PayloadAction<[]>) => state.albums = action.payload
+    }
+});
+
+export const {set} = albumSlice.actions
+
+export default albumSlice.reducer
