2a249dbe1b19628a054924507b080c151c5b7926
mhmddev <mabrahimibagha@gmail.com>
2023-06-06 16:56:11 +0330

    feat: add remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**.scdn.co'
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
