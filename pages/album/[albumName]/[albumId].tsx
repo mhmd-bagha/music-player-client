3a5f2675c87c28dfbb475efb26c514ffcb5a23be
mhmddev <mabrahimibagha@gmail.com>
2023-06-11 23:17:29 +0330

    feat(type): add song type

--- pages/album/[albumName]/[albumId].tsx
+++ pages/album/[albumName]/[albumId].tsx
@@ -4,6 +4,8 @@ import Songs from "Component/artist/songs";
 import PLayerView from "Component/player/player";
 import {useGlobalContext} from "@/context/store";
 import {useEffect} from "react";
+import {GetServerSideProps} from "next";
+import context from "react-redux/src/components/Context";
 
 const Album = () => {
     const {setShowSidebar} = useGlobalContext()
@@ -24,6 +26,20 @@ const Album = () => {
             <PLayerView/>
         </>
     )
+}
+
+export const getServerSideProps = (context) => {
+    const {} = context.params
+
+    console.log(albumId)
+
+    const songs = [{}]
+
+    return {
+        props: {
+            songs
+        }
+    }
 }
 
 export default Album
