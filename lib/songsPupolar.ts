88601c32e9afb188d785012e2e205f696295c80b
mhmddev <mabrahimibagha@gmail.com>
2023-06-18 18:20:07 +0330

    refactor(api): add export

--- lib/songsPupolar.ts
+++ lib/songsPupolar.ts
@@ -1,19 +1,19 @@
 import axiosInstance from './axios';
 import {setLike} from "@/redux/reducers/songs-popular";
 
-const getSongsLiked = async () => {
+export const getSongsLiked = async () => {
     const res = await axiosInstance.post('/album/songs/like');
 
     return res.data
 }
 
-const addSongLike = async (songId) => {
+export const addSongLike = async (songId) => {
     const res = await axiosInstance.post('/album/song/like/add', songId);
 
     return (res.data === 200) ? setLike(songId) : res.data
 }
 
-const removeSongLike = async () => {
+export const removeSongLike = async () => {
     const res = await axiosInstance.post('/album/song/like/remove');
 
     return res.data
