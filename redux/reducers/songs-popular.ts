5a3cf9a4a00c72e7c2ad82f9724d51b6fd2af0ef
mhmddev <mabrahimibagha@gmail.com>
2023-06-18 18:09:58 +0330

    feat(redux): add songs pupolar

---
+++ redux/reducers/songs-popular.ts
@@ -0,0 +1,23 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+interface InitialState {
+    songs: []
+}
+
+const initialState: InitialState = {
+    songs: []
+}
+
+const songsPopularSlice = createSlice({
+    name: 'songPopular',
+    initialState,
+    reducers: {
+        setLike: (state, action: PayloadAction<number>) => {
+            state.songs = state.songs.push(action.payload)
+        }
+    }
+})
+
+export const {setLike} = songsPopularSlice.actions
+
+export default songsPopularSlice.reducer
