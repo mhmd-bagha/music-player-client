faf2c0e82dec589b98c32f76145237f0e92895a6
mhmddev <mabrahimibagha@gmail.com>
2023-06-19 15:43:31 +0330

    refactor(api): return only status code

--- lib/songsPupolar.ts
+++ lib/songsPupolar.ts
@@ -10,7 +10,7 @@ export const getSongsLiked = async () => {
 export const addSongLike = async (songId) => {
     const res = await axiosInstance.post('/album/song/like/add', {song_id: songId});
 
-    return (res.data === 200) ? setLike(songId) : res.data
+    return res.status
 }
 
 export const removeSongLike = async () => {
